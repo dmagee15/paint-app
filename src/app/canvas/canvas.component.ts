7cd5aabee63464a11b66a29d60c7e6aa5c27046f
dmagee15 <david.magee15@yahoo.com>
2018-05-05 09:19:08 -0600

    added basic drawing functionality

--- src/app/canvas/canvas.component.ts
+++ src/app/canvas/canvas.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-canvas',
@@ -8,17 +8,41 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 export class CanvasComponent implements OnInit {
   @ViewChild('theCanvas') theCanvas;
   ctx = null;
+  mouseClicked = false;
+
   constructor() { }
 
   ngOnInit() {
+    this.theCanvas.nativeElement.width = document.documentElement.clientWidth;
+    this.theCanvas.nativeElement.height = document.documentElement.clientHeight;
     this.ctx = this.theCanvas.nativeElement.getContext("2d");
   }
 
-  onClick(event){
-    console.log(event.screenX+" ",event.screenY);
-    console.log(event.clientX+" ",event.clientY);
-    this.ctx.fillStyle = "#FF0000";
-    this.ctx.fillRect(event.clientX,event.clientY,8,5);
+  @HostListener('document:mousemove', ['$event']) 
+  onMouseMove(e) {
+    console.log(e);
+    if(this.mouseClicked == true){
+      this.ctx.lineTo(e.clientX,e.clientY);
+      this.ctx.stroke(); // Draw it
+    }
+  }
+
+  onMouseDown(event){
+    this.mouseClicked = true;
+//    this.ctx.fillStyle = "#FF0000";
+//    this.ctx.fillRect(event.clientX,event.clientY,8,8);
+    this.ctx.beginPath(); 
+    this.ctx.lineWidth="5";
+    this.ctx.strokeStyle="green"; // Green path
+    this.ctx.moveTo(event.clientX,event.clientY);
+  }
+
+  onMouseUp(event){
+    this.mouseClicked = false;
+  }
+
+  onMouseLeave(event){
+    this.mouseClicked = false;
   }
 
 }
